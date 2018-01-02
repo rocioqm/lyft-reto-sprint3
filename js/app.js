cb00552c0e2937a6434b35fa6a706d917b247f9d
rocioqm <rocio.quilla@aiesec.net>
2018-01-01 23:31:26 -0500

    signup view

---
+++ js/app.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  setTimeout(function() {
+    window.location.href = 'file:///C:/Users/CHIO/Desktop/LABORATORIA/lyft-reto-sprint3/views/startview.html';
+  }, 4000);
+});
+
+$('.btn-start').click(function() {
+  window.location.href = 'file:///C:/Users/CHIO/Desktop/LABORATORIA/lyft-reto-sprint3/views/signupview.html';
+});
+
